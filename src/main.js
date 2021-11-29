a3f372eda7fbd18256159209b7f1fd196ed7832d
GTF_Apollo <ceruleanpaladin@gmail.com>
2021-11-29 22:51:07 +0100

    Setup Fontawesome, Dicier, TailwindCSS

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import '~/assets/styles.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
